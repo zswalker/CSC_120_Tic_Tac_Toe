8cc6266e0a79e36e09a0240ca8f872620e0f48cc
zswalker <zswalker@my.waketech.edu>
2022-04-15 14:01:28 -0400

    Board Printing functionality added

--- board.py
+++ board.py
@@ -4,11 +4,11 @@ row2 = ['[-]', '[-]', '[-]']
 board = [row0, row1, row2]
 
 
-def printBoard():
+def print_board():
     print("Printing board")
     print(board[0][0], board[0][1], board[0][2])
     print(board[1][0], board[1][1], board[1][2])
     print(board[2][0], board[2][1], board[2][2])
 
 
-printBoard()
+print_board()
